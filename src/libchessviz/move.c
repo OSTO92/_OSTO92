8e59d119b071c18cfff150ed7a75f3dc8be481c3
Boyarintsev Danila <logotip-228@mail.ru>
2022-03-30 18:48:50 +0700

    move.c

---
+++ src/libchessviz/move.c
@@ -0,0 +1,96 @@
+#include "move.h"
+#include <libchessviz/print_field.h>
+#include <stdio.h>
+#include <string.h>
+
+int move(char r_move[10], char chess_field[8][8])
+{
+    if (r_move[0] >= 97 && r_move[0] <= 104) 
+    {
+        char row1 = r_move[1] - 49, row2 = r_move[4] - 49;
+        char column1 = r_move[0] - 97, column2 = r_move[3] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'P' && tmp != 'p') 
+	{
+            printf("Error: it's not a pawn!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+
+    if (r_move[0] == 'K'|| r_move[0] == 'k') 
+    {
+        char row1 = r_move[2] - 49, row2 = r_move[5] - 49;
+        char column1 = r_move[1] - 97, column2 = r_move[4] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'K' && tmp != 'k') 
+        {
+            printf("Error: it's not a king!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+    if (r_move[0] == 'Q'|| r_move[0] == 'q') 
+    {
+        char row1 = r_move[2] - 49, row2 = r_move[5] - 49;
+        char column1 = r_move[1] - 97, column2 = r_move[4] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'Q' && tmp != 'q') 
+        {
+            printf("Error: it's not a queen!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+    if (r_move[0] == 'R'|| r_move[0] == 'r') 
+    {
+        char row1 = r_move[2] - 49, row2 = r_move[5] - 49;
+        char column1 = r_move[1] - 97, column2 = r_move[4] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'R' && tmp != 'r') 
+        {
+            printf("Error: it's not a rook!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+    if (r_move[0] == 'N'|| r_move[0] == 'n') 
+    {
+        char row1 = r_move[2] - 49, row2 = r_move[5] - 49;
+        char column1 = r_move[1] - 97, column2 = r_move[4] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'N' && tmp != 'n') 
+        {
+            printf("Error: it's not a knight!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+    if (r_move[0] == 'B'|| r_move[0] == 'b') 
+    {
+        char row1 = r_move[2] - 49, row2 = r_move[5] - 49;
+        char column1 = r_move[1] - 97, column2 = r_move[4] - 97;
+        char tmp = chess_field[7 - row1][(int)column1];
+        if (tmp != 'B' && tmp != 'b') 
+        {
+            printf("Error: it's not a bishop!\n");
+            return 1;
+        }
+        chess_field[7 - row1][(int)column1] = 32;
+        chess_field[7 - row2][(int)column2] = tmp;
+    }
+
+
+
+    print_field(chess_field);
+    if (r_move[strlen(r_move) - 1] == '#')
+    {
+	return 10;
+    }
+    return 0;
+}
